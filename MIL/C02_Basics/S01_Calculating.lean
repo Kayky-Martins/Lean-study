509572b0f099ee534c7383204ad1e5872726bcd6
Kayky-Martins <lopeskayky641@gmail.com>
2026-06-03 00:43:24 -0300

    comeco do MIL e Reuniao4.lean

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -43,10 +43,14 @@ example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c *
   rw [mul_assoc]
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  sorry
+  rw[mul_assoc a,h,← mul_assoc a]
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  calc
+     c = b * a - d := by rw[hyp]
+     _ = b*a -a*b:= by rw[hyp']
+     _ = b*a - b*a:= by rw[mul_comm a b]
+     _ = 0:= by rw[sub_self]
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
@@ -91,14 +95,15 @@ example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
     _ = a * a + 2 * (a * b) + b * b := by
       rw [mul_comm b a, ← two_mul]
 
+--**
 example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
   calc
     (a + b) * (a + b) = a * a + b * a + (a * b + b * b) := by
-      sorry
+      rw[mul_add,add_mul,add_mul]
     _ = a * a + (b * a + a * b) + b * b := by
-      sorry
+      rw [← add_assoc,add_assoc (a * a) (b * a)]
     _ = a * a + 2 * (a * b) + b * b := by
-      sorry
+      rw[mul_comm a b,two_mul]
 
 end
 
@@ -107,7 +112,9 @@ section
 variable (a b c d : ℝ)
 
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  rw[mul_add,add_mul,add_mul]
+  rw [← add_assoc,add_assoc (a * c) (b * c),add_comm (b*c)]
+  rw[← add_assoc]
 
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
   sorry
